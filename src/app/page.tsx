b0bfa93454712fad769e97b2db82af68a25f7cb8
Junhao Zhang <jerry_mountain@hotmail.com>
2026-01-03 00:28:32 -0600

    added icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import BlurBackground from "@/components/BlurBackground";
 import { serif } from "./fonts";
 import Link from "next/link";
 import { Button } from "@/components/Button";
+import { MdEmail } from "react-icons/md";
+import { FaFileAlt, FaGithub, FaLinkedin } from "react-icons/fa";
 
-function ButtonLink({ children }: React.PropsWithChildren) {
+function ButtonLink({ children, href }: { href: string } & React.PropsWithChildren) {
     return (
-        <div className="p-2 border-1 border-zinc-300 my-2 rounded">
-            {children}
-        </div>
+        <Link href={href} target="blank_">
+            <div className="p-2 border-1 border-zinc-300 my-2 rounded">
+                {children}
+            </div>
+        </Link>
     );
 }
 
@@ -31,21 +35,23 @@ export default function Main() {
                             border: "1px solid rgba(255, 255, 255, 0.5)",
                         }}>
                             <h1 className="font-bold text-zinc-400">Quick Links</h1>
-                            <ButtonLink>
-                                <Link href="/resume.pdf" target="_blank">Resume</Link>
+                            <ButtonLink href="/resume.pdf">
+                                <FaFileAlt className="inline-block m-2" />
+                                Resume
                             </ButtonLink>
-                            <ButtonLink>
-                                <Link href="https://github.com/cursorweb" target="_blank">GitHub</Link>
+                            <ButtonLink href="https://github.com/cursorweb">
+                                <FaGithub className="inline-block m-2" />
+                                GitHub
                             </ButtonLink>
-                            <ButtonLink>
-                                <Link href="https://www.linkedin.com/in/jzhang2029/" target="_blank">Linkedin</Link>
+                            <ButtonLink href="https://www.linkedin.com/in/jzhang2029/">
+                                <FaLinkedin className="inline-block m-2" />
+                                Linkedin
                             </ButtonLink>
-                            <ButtonLink>
-                                <Link href="mailto:jzhang1707@outlook.com">Email</Link>
+                            <ButtonLink href="mailto:jzhang1707@outlook.com">
+                                <MdEmail className="inline-block m-2" />
+                                Email
                             </ButtonLink>
-                            <ButtonLink>
-                                <Button />
-                            </ButtonLink>
+                            <Button />
                         </div>
                     </div>
                     <div>Down arrow</div>
