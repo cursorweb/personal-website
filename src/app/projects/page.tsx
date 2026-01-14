12364b6e3f1275672d55b8105542934ece091be7
Junhao Zhang <jerry_mountain@hotmail.com>
2026-01-13 21:52:36 -0600

    Added ray tracer project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,6 +3,8 @@ import { serif } from "../fonts";
 import { Card } from "@/components/Card";
 import BlurBackground from "@/components/BlurBackground";
 import Link from "next/link";
+import { FaDiceD20, FaGithub, FaRocket, FaYoutube } from "react-icons/fa";
+import { MdBlurOn } from "react-icons/md";
 
 export default function Projects() {
     return (
@@ -21,26 +23,40 @@ export default function Projects() {
             </h1>
 
             <div className="flex flex-col gap-5 mx-20">
-                <Card>
-                    {/* carousel */}
-                    <img src="/assets/rtspheres.png" />
-                    <div className="text-lg">RusTX</div>
-                    <div>
+                <div className="p-5 rounded-xl border border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-950 flex flex-row shadow-lg">
+                    <div className="flex-1">
+                        {/* carousel */}
+                        <img src="/assets/rtspheres.png" className="shadow-lg" />
+                    </div>
+                    <div className="flex-1">
+                        <div className="text-4xl font-bold text-center my-5 text-shadow-lg text-blue-700">RusTX</div>
                         I've used Blender a lot, which use ray tracing software to render models and I wanted to better understand how ray tracing worked, as well as learn the technical details behind the algorithms.
                         So, for the CS128 Honors final project, I led a team of three to build a ray tracer in Rust.
                         Since a lot of the ray tracing codebase was written in C++, we were able to compare the benefits of using Rust over C++ as well as
                         learn more about multi-threaded Rust.
 
-                        <div className="text-lg font-bold">Features</div>
-                        <ul>
-                            <li>Multi-threading</li>
-                            <li>STL model rendering</li>
-                            <li>Glass, metallic, and lambertian materials</li>
-                        </ul>
+                        <div className="my-4">
+                            <div className="text-lg font-bold">Features</div>
+                            <ul>
+                                <li><FaRocket className="inline mr-2 text-orange-500" aria-hidden /> Multi-threading</li>
+                                <li><FaDiceD20 className="inline mr-2 text-green-600" aria-hidden /> STL model parsing and rendering</li>
+                                <li><MdBlurOn className="inline mr-2 text-blue-500" aria-hidden /> Glass, metallic, and lambertian materials</li>
+                            </ul>
+                        </div>
+                        <Link
+                            href="https://github.com/pranavpopuri/raytracing-in-rust/" target="_blank"
+                            className="rounded-full bg-blue-500 hover:bg-blue-700 transition py-1 px-5 inline-block text-white mr-2"
+                        >
+                            <FaGithub className="inline" /> Github
+                        </Link>
+                        <Link
+                            href="https://www.youtube.com/watch?v=4iuJ3HSaAr0" target="_blank"
+                            className="rounded-full bg-red-500 hover:bg-red-700 transition py-1 px-5 inline-block text-white"
+                        >
+                            <FaYoutube className="inline" /> Walkthrough
+                        </Link>
                     </div>
-                    <Link href="https://github.com/pranavpopuri/raytracing-in-rust/">Github</Link>
-                    <Link href="https://www.youtube.com/watch?v=4iuJ3HSaAr0">Walkthrough Video</Link>
-                </Card>
+                </div>
                 <div className="font-mono p-5 rounded-xl border border-gray-600 bg-[#f9f3f0] dark:bg-[#1e1e1e]">
                     <div className="flex flex-row gap-5 mb-5">
                         <div>
@@ -49,14 +65,14 @@ export default function Projects() {
                         <div>
                             <div className="text-2xl mb-2 text-[#569cd6]">&gt; Europa Lang</div>
                             I've always been fascinated by how programming languages <span className="italic dark:text-[#b5cea8] text-[#098658]">themselves</span> worked.
-                            Learning from the popular <Link href="https://craftinginterpreters.com" className="dark:text-[#c586c0] text-[#af00db] underline hover:no-underline">[ craftinginterpreters ]</Link> tutorial,
+                            Learning from the popular <Link href="https://craftinginterpreters.com" className="dark:text-[#c586c0] text-[#af00db] underline hover:no-underline" target="_blank">[ craftinginterpreters ]</Link> tutorial,
                             I created <span className="dark:text-[#dcdcaa] text-[#795e26]">Europa Lang</span>, inspired by the syntax of <span className="dark:text-[#9cdcfe] text-[#001080]">Rust</span> and <span className="dark:text-[#9cdcfe] text-[#001080]">JavaScript</span>.
                             It's grown to have over <span className="dark:text-[#4ec9b0] text-[#267f99]">20 stars</span> on GitHub and has had multiple people contribute to the project!
                         </div>
                     </div>
                     <div>
-                        <div className="mb-2 dark:text-[#6a9955] text-[#008000]">/** <span className="text-[#569cd6]">Features</span></div>
-                        <ul className="list-['*_'] ml-10 dark:text-[#6a9955] text-[#008000]">
+                        <div className="dark:text-[#6a9955] text-[#008000]">/** <span className="text-[#569cd6]">Features</span></div>
+                        <ul className="list-['*_'] ml-9 dark:text-[#6a9955] text-[#008000]">
                             <li>Recursive-descent parsing</li>
                             <li>Feature-rich standard library</li>
                             <li>Support for arrays, ranges, and hashmaps</li>
@@ -64,8 +80,8 @@ export default function Projects() {
                         <div className="mb-2 ml-5 dark:text-[#6a9955] text-[#008000]">*/</div>
                     </div>
                     <div className="flex gap-2">
-                        <Link href="https://github.com/europalang/Europa-Lang" className="dark:text-[#4ec9b0] text-[#267f99] underline hover:no-underline">[ GitHub ]</Link>
-                        <Link href="https://marketplace.visualstudio.com/items?itemName=cursorweb.europalang-highlighter" className="dark:text-[#569cd6] text-[#795e26] underline hover:no-underline">[ VSCode Extension ]</Link>
+                        <Link href="https://github.com/europalang/Europa-Lang" target="_blank" className="dark:text-[#4ec9b0] text-[#267f99] underline hover:no-underline">[ GitHub ]</Link>
+                        <Link href="https://marketplace.visualstudio.com/items?itemName=cursorweb.europalang-highlighter" target="_blank" className="dark:text-[#569cd6] text-[#795e26] underline hover:no-underline">[ VSCode Extension ]</Link>
                     </div>
                 </div>
                 <Card>
@@ -87,9 +103,9 @@ export default function Projects() {
                             <li>Over 20 levels</li>
                         </ul>
                     </div>
-                    <Link href="https://github.com/CursorwebGames/2DExtremeMiniGolf/">GitHub</Link>
-                    <Link href="https://cursorwebgames.github.io/2DExtremeMiniGolf/">Game</Link>
-                    <Link href="https://cursorwebgames.github.io/2DExtremeMiniGolf/level-editor/">Level Editor</Link>
+                    <Link href="https://github.com/CursorwebGames/2DExtremeMiniGolf/" target="_blank">GitHub</Link>
+                    <Link href="https://cursorwebgames.github.io/2DExtremeMiniGolf/" target="_blank">Game</Link>
+                    <Link href="https://cursorwebgames.github.io/2DExtremeMiniGolf/level-editor/" target="_blank">Level Editor</Link>
                 </Card>
                 {/* <Card>
                     <div className="text-lg">Cycle Bot</div>
