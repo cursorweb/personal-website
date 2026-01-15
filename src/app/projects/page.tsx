7da4aab6353d4e39879a56f17b9652eb24b80b61
Junhao Zhang <jerry_mountain@hotmail.com>
2026-01-15 17:38:17 -0600

    finished design for vector golf
    might change later

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -90,27 +90,38 @@ export default function Projects() {
                     </div>
 
                     {/* Vector Golf */}
-                    <div className="relative p-7 pb-0 bg-black rounded-2xl">
-                        <div className="p-5 bg-white">
-                            <img src="/assets/vectorgolf.png" alt="Vector Golf" width={50} />
-                            <div className="text-lg">Vector Golf</div>
-                            I am a huge physics fan, and I really enjoyed my experience in AP Physics 1 & 2.
-                            Being able to model the physics equations to create realistic simulations will never cease to amaze me.
-                            It's also inspired me to create a golf game! Taking inspiration from GamePigeon golf, I created a unique mini-golf experience:
-                            There are ice blocks which break if you hit them, portals that teleport your ball, and so much more!
-                            To streamline level development, I also made a fully-featured level editor that lets you import and export custom levels.
-                            The game is still in development, but it works both on mobile and desktop.
+                    <div className="p-7 pb-0 bg-black rounded-2xl">
+                        <div className="relative overflow-hidden">
+                            <img src="/assets/vectorgolf.png" className="absolute top-0 left-0 w-full z-1" alt="Vector Golf" />
 
-                            <div className="text-lg font-bold">Features</div>
-                            <ul className="list-disc ml-5">
-                                <li>Fully-featured Level Editor</li>
-                                <li>P5.js with Typescript + Vite pipeline</li>
-                                <li>Custom hand-built physics engine</li>
-                                <li>Over 20 levels</li>
-                            </ul>
-                            <Link href="https://github.com/CursorwebGames/2DExtremeMiniGolf/" target="_blank">GitHub</Link>
-                            <Link href="https://cursorwebgames.github.io/2DExtremeMiniGolf/" target="_blank">Game</Link>
-                            <Link href="https://cursorwebgames.github.io/2DExtremeMiniGolf/level-editor/" target="_blank">Level Editor</Link>
+                            <div className="relative bg-linear-to-b from-transparent to-50% to-white z-2 p-5 pt-65">
+                                <div className="text-2xl">Vector Golf</div>
+                                I love physics, especially creating realistic simulations. So, taking inspiration from GamePigeon Golf, I made my own golf game:
+                                there are ice blocks which break if you hit them, portals that teleport your ball, and so much more!
+                                The game includes a custom hand-written physics engine, a fully functioning level editor, and works on desktop and mobile.
+
+                                <div className="text-lg font-bold">Features</div>
+                                <ul className="list-disc ml-5">
+                                    <li>Fully-featured Level Editor</li>
+                                    <li>P5.js with Typescript + Vite pipeline</li>
+                                    <li>Custom hand-built physics engine</li>
+                                    <li>Over 20 levels</li>
+                                </ul>
+                                <Link
+                                    href="https://github.com/CursorwebGames/2DExtremeMiniGolf/" target="_blank"
+                                    className="inline-block px-2 border-2 border-blue-500 hover:bg-blue-500 hover:text-white m-2 mx-1"
+                                >
+                                    GitHub
+                                </Link>
+                                <Link
+                                    href="https://cursorwebgames.github.io/2DExtremeMiniGolf/" target="_blank"
+                                    className="inline-block px-2 border-2 border-green-600 hover:bg-green-600 hover:text-white m-2 mx-1"
+                                >Game</Link>
+                                <Link
+                                    href="https://cursorwebgames.github.io/2DExtremeMiniGolf/level-editor/" target="_blank"
+                                    className="inline-block px-2 border-2 border-orange-500 hover:bg-orange-500 hover:text-white m-2 mx-1"
+                                >Level Editor</Link>
+                            </div>
                         </div>
 
                         <div className="flex justify-center my-2">
