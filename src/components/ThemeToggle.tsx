eef44ad853b445ed12b9414db06b2b3b5d91d2cc
Junhao "Jerry" Zhang <jerry_mountain@hotmail.com>
2026-06-16 22:33:17 -0500

    Add favicon (#5)
    * Fix system theme default
    
    When user has system theme to dark by default, you have to click twice. Now you don't.
    
    * add logo!
    
    * Add icons
    
    * Clean up code
    
    * Put gen-logo.html in a safe place

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -6,7 +6,7 @@ import { FaCloud, FaMoon } from "react-icons/fa";
 import { MdSunny } from "react-icons/md";
 
 export function ThemeToggle() {
-    const { theme, setTheme } = useTheme();
+    const { resolvedTheme, setTheme } = useTheme();
 
     const [mounted, setMounted] = useState(false);
 
@@ -21,11 +21,11 @@ export function ThemeToggle() {
 
     return (
         <button
-            onClick={() => setTheme(t => t == "dark" ? "light" : "dark")}
+            onClick={() => setTheme(resolvedTheme == "dark" ? "light" : "dark")}
             className="transition-colors cursor-pointer text-gray-500 dark:text-gray-400 hover:text-black hover:dark:text-white"
             aria-label="Change theme">
-            {theme == "dark" ? <FaMoon className="text-4xl md:text-lg inline-block mb-3 md:mb-1" /> : <MdSunny className="text-4xl md:text-lg inline-block mb-3 md:mb-1" />}
-            <span className="text-4xl md:hidden ml-2">{theme == "dark" ? "Dark" : "Light"}</span>
+            {resolvedTheme == "dark" ? <FaMoon className="text-4xl md:text-lg inline-block mb-3 md:mb-1" /> : <MdSunny className="text-4xl md:text-lg inline-block mb-3 md:mb-1" />}
+            <span className="text-4xl md:hidden ml-2">{resolvedTheme == "dark" ? "Dark" : "Light"}</span>
         </button>
     );
 }
